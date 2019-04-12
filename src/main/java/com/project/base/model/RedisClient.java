bc11be39e3804cb60e62e2a7fcd628086d9b0f5b
602377321@qq.com <sbjg.824711>
2019-04-12 18:18:00 +0800

    update

---
+++ src/main/java/com/project/base/model/RedisClient.java
@@ -0,0 +1,121 @@
+package com.project.base.model;
+
+import org.apache.commons.lang3.StringUtils;
+import redis.clients.jedis.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: RedisClient
+ * @Description:
+ * @author: yinshaobo
+ * @date: 2019/4/12 17:50
+ */
+public class RedisClient {
+
+    /**
+     * 非切片额客户端连接
+     */
+    private Jedis jedis;
+
+    /**
+     * 非切片连接池
+     */
+    private JedisPool jedisPool;
+
+    /**
+     * 切片额客户端连接
+     */
+    private ShardedJedis shardedJedis;
+
+    /**
+     * 切片连接池
+     */
+    private ShardedJedisPool shardedJedisPool;
+
+    private String host;
+
+    private Integer port;
+
+    private String password;
+
+    public RedisClient() {
+        this.host = "127.0.0.1";
+        this.port = 6379;
+        this.init();
+    }
+
+    public RedisClient(String host, Integer port, String password) {
+        this.host = host;
+        this.port = port;
+        this.password = password;
+        this.init();
+    }
+
+    private void init () {
+        initialPool();
+        initialShardedPool();
+        shardedJedis = shardedJedisPool.getResource();
+        jedis = jedisPool.getResource();
+    }
+
+    public Jedis getJedis() {
+        return jedis;
+    }
+
+    public void setJedis(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public ShardedJedis getShardedJedis() {
+        return shardedJedis;
+    }
+
+    public void setShardedJedis(ShardedJedis shardedJedis) {
+        this.shardedJedis = shardedJedis;
+    }
+
+    /**
+     * 初始化非切片池
+     */
+    private void initialPool() {
+        // 池基本配置
+        JedisPoolConfig config = new JedisPoolConfig();
+        //可用连接实例的最大数目，默认值为8
+        //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+        config.setMaxTotal(1024);
+        //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
+        config.setMaxWaitMillis(10 * 1000L);
+        //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+        config.setMaxIdle(200);
+        //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+        config.setTestOnBorrow(false);
+        if (StringUtils.isNotBlank(this.password)) {
+            jedisPool = new JedisPool(config, this.host, this.port, 10 * 1000, this.password);
+        } else {
+            jedisPool = new JedisPool(config, this.host, this.port);
+        }
+    }
+
+    /**
+     * 初始化切片池
+     */
+    private void initialShardedPool() {
+        // 池基本配置
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(1024);
+        config.setMaxWaitMillis(10000L);
+        config.setMaxIdle(200);
+        config.setTestOnBorrow(false);
+        // slave链接
+        List<JedisShardInfo> shards = new ArrayList<>();
+        JedisShardInfo jedisShardInfo = new JedisShardInfo("127.0.0.1", 6379, "master");
+        if (StringUtils.isNotBlank(this.password)) {
+            jedisShardInfo.setPassword(this.password);
+        }
+        shards.add(jedisShardInfo);
+        // 构造池
+        shardedJedisPool = new ShardedJedisPool(config, shards);
+    }
+}
--- src/main/java/com/project/base/service/impl/RedisServiceImpl.java
+++ src/main/java/com/project/base/service/impl/RedisServiceImpl.java
@@ -1,49 +1,36 @@
 package com.project.base.service.impl;
 
 
-import com.project.base.model.Constants;
+import com.project.base.model.RedisClient;
 import com.project.base.service.IRedisService;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.ShardedJedis;
 
 public class RedisServiceImpl implements IRedisService {
 
     private static Logger logger = LoggerFactory.getLogger(RedisServiceImpl.class);
 
-    private static Jedis jedis;
+    private Jedis jedis;
 
-    /**
-     * 初始化Redis连接池
-     */
-    static {
-        JedisPoolConfig config = new JedisPoolConfig();
-        //可用连接实例的最大数目，默认值为8
-        //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-        config.setMaxTotal(1024);
-        //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
-        config.setMaxIdle(200);
-        //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
-        config.setMaxWaitMillis(10000);
-        //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
-        config.setTestOnBorrow(true);
-        JedisPool jedisPool;
-        if (StringUtils.isNoneBlank(Constants.getRedisPassword())) {
-            jedisPool = new JedisPool(config, Constants.getRedisHost(), Constants.getRedisPort(), 10000, Constants.getRedisPassword());
-        } else {
-            jedisPool = new JedisPool(config, Constants.getRedisHost(), Constants.getRedisPort(), 10000);
-        }
-        jedis = jedisPool.getResource();
-        logger.info("init common redis success... ...");
+    private ShardedJedis shardedJedis;
+
+    public RedisServiceImpl() {
+        RedisClient redisClient = new RedisClient();
+        this.jedis = redisClient.getJedis();
+        this.shardedJedis = redisClient.getShardedJedis();
+    }
+
+    public RedisServiceImpl(String host, Integer port, String password) {
+        RedisClient redisClient = new RedisClient(host, port, password);
+        this.jedis = redisClient.getJedis();
+        this.shardedJedis = redisClient.getShardedJedis();
     }
 
     @Override
     public void set(String key, String value) {
-        jedis.set(key, value);
-        jedis.close();
+        this.jedis.set(key, value);
     }
 
     @Override
