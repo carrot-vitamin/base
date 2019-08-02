c4b4aec026168bae35c4cc2a65116391ff089ff1
mac <602377321@qq.com>
2019-08-02 10:36:03 +0800

    Revert "update"
    This reverts commit 70d92ca3c753fce4a2e125a79a427f4a9e9320a2.

--- src/main/java/com/project/base/MainTest.java
+++ src/main/java/com/project/base/MainTest.java
@@ -2,29 +2,21 @@ package com.project.base;
 
 import com.project.base.service.IRedisService;
 import com.project.base.service.impl.RedisServiceImpl;
+import com.project.base.util.AESUtils;
+import com.project.base.util.KeyGenerator;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainTest {
 
     private static IRedisService redisService = new RedisServiceImpl("test", "0okm(IJN");
 
     public static void main(String[] args) throws Exception {
-        Map<String, Person> map = new HashMap<>(2);
-        map.put("k1", new Person("张三"));
-        map.put("k2", new Person("张三1"));
-        redisService.hmset("key1", map);
-        System.out.println(redisService.hGetAll("key1"));
-        redisService.hPut("key1", "k5", new Person("vvv"));
-        System.out.println(redisService.hGetAll("key1"));
-        redisService.hDel("key1", "k2");
-//        sleep(5);
-        System.out.println(redisService.hGetAll("key1"));
-
-        Person p = redisService.hGet("key1", "k5");
-        System.out.println(p);
+        String key = KeyGenerator.generate();
+        System.out.println("WEo5o93xhAiZkpzRH4NpZQ==\n");
+        for (int i = 0; i < 20; i ++) {
+            System.out.println(AESUtils.encrypt("1234567", key));
+        }
     }
 
     private static void sleep(int seconds) throws Exception {
