467e7bf4539969b10b5028638b67c42c0619c949
mac <602377321@qq.com>
2019-08-02 10:36:31 +0800

    Revert "update"
    This reverts commit 70d92ca3

--- src/main/java/com/project/base/util/AESUtils.java
+++ src/main/java/com/project/base/util/AESUtils.java
@@ -1,11 +1,15 @@
 package com.project.base.util;
 
-import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.Charset;
+import java.security.SecureRandom;
 
 /**
  * @author yin
@@ -13,48 +17,92 @@ import java.nio.charset.Charset;
  */
 public class AESUtils {
 
-    /**
-     * 加密盐值
-     */
-    private static final String SALT_VALUE = "MG9rbShJSk4JytHf";
+    private static Logger log = LoggerFactory.getLogger(AESUtils.class);
 
     /**
-     * 加密明文字符串
-     * @param data 要加密的数据
-     * @param key 密钥
-     * @return 加密后的数据
+     * 加密
+     * 1.构造密钥生成器
+     * 2.根据aesKey初始化密钥生成器
+     * 3.产生密钥
+     * 4.创建和初始化密码器
+     * 5.内容加密
+     * 6.返回字符串
+     *
+     * @param aesKey aesKey
+     * @param content content
+     * @return string
      */
-    public static String encrypt(String key, String data) throws Exception {
-        byte[] bytes = encryptBytes(data.getBytes(Charset.defaultCharset()), key.getBytes(Charset.defaultCharset()));
-        return Base64.encodeBase64String(bytes);
+    public static String encrypt(String aesKey, String content) {
+        try {
+            Object [] objects = getCipher(aesKey);
+            Cipher cipher = (Cipher) objects[0];
+            SecretKey key = (SecretKey) objects[1];
+            //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密解密(Decrypt_mode)操作，第二个参数为使用的KEY
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            //8.获取加密内容的字节数组(这里要设置为utf-8)不然内容中如果有中文和英文混合中文就会解密为乱码
+            byte[] byteEncode = content.getBytes("utf-8");
+            //9.根据密码器的初始化方式--加密：将数据加密
+            byte[] byteAES = cipher.doFinal(byteEncode);
+            //10.将加密后的数据转换为字符串
+            //这里用Base64Encoder中会找不到包
+            //解决办法：
+            //在项目的Build path中先移除JRE System Library，再添加库JRE System Library，重新编译后就一切正常了。
+            return new BASE64Encoder().encode(byteAES);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
     }
 
     /**
-     * 解密密文字符串
-     * @param data 要解密的数据
-     * @param key 密钥
-     * @return 解密后的数据
+     * 解密
+     * 1.同加密1-4步
+     * 2.将加密后的字符串反纺成byte[]数组
+     * 3.将加密内容解密
+     * @param aesKey aesKey
+     * @param content content
+     * @return string
      */
-    public static String decrypt(String key, String data) throws Exception {
-        byte[] bytes = decryptBytes(Base64.decodeBase64(data), key.getBytes(Charset.defaultCharset()));
-        return new String(bytes, Charset.defaultCharset());
+    public static String decrypt(String aesKey, String content) {
+        try {
+            Object [] objects = getCipher(aesKey);
+            Cipher cipher = (Cipher) objects[0];
+            SecretKey key = (SecretKey) objects[1];
+            //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            //8.将加密并编码后的内容解码成字节数组
+            byte[] byteContent = new BASE64Decoder().decodeBuffer(content);
+            /*
+             * 解密
+             */
+            byte[] byteDecode = cipher.doFinal(byteContent);
+            return new String(byteDecode, "utf-8");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
     }
 
-
-    private static byte[] encryptBytes(byte[] data, byte[] keyBytes) throws Exception {
-        return cipherOpt(data, keyBytes, Cipher.ENCRYPT_MODE);
-    }
-
-    private static byte[] decryptBytes(byte[] data, byte[] keyBytes) throws Exception {
-        return cipherOpt(data, keyBytes, Cipher.DECRYPT_MODE);
-    }
-
-    private static byte[] cipherOpt(byte[] dataBytes, byte[] keyBytes, int mode) throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(SALT_VALUE.getBytes(Charset.defaultCharset()));
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(mode, secretKey, ivParameterSpec);
-        return cipher.doFinal(dataBytes);
+    private static Object[] getCipher(String aesKey) {
+        try {
+            //1.构造密钥生成器，指定为AES算法,不区分大小写
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            //2.根据ecnodeRules规则初始化密钥生成器
+            //生成一个128位的随机源,根据传入的字节数组
+            keygen.init(128, new SecureRandom(aesKey.getBytes()));
+            //3.产生原始对称密钥
+            SecretKey secretKey = keygen.generateKey();
+            //4.获得原始对称密钥的字节数组
+            byte[] raw = secretKey.getEncoded();
+            //5.根据字节数组生成AES密钥
+            SecretKey key = new SecretKeySpec(raw, "AES");
+            //6.根据指定算法AES自成密码器
+            Cipher cipher = Cipher.getInstance("AES");
+            return new Object [] {cipher, key};
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return null;
+        }
     }
 
 }
