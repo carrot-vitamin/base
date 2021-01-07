6de56803a42d0fdbd5b02c02e5bcd40f7eee4133
za-yinshaobo <yinshaobo@zhongnan.io>
2021-01-07 18:06:39 +0800

    以流的方式读取resource下的文件

--- src/main/java/com/project/base/MainTest.java
+++ src/main/java/com/project/base/MainTest.java
@@ -1,41 +1,11 @@
 package com.project.base;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.project.base.util.CacheUtils;
+import com.project.base.util.LocationUtils;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class MainTest {
 
-    @JsonProperty(value = "abc")
-    private String name;
-
-    public MainTest(String name) {
-        this.name = name;
-    }
-
     public static void main(String[] args) throws Exception {
-        CacheUtils.resetFilePath("C:\\Users\\shaob\\Downloads");
-
-        CacheUtils.set("k1", "v1", 5);
-
-        String v = CacheUtils.get("k1");
-        System.out.println("1================" + v);
-
-        Thread.sleep(8000);
-
-        String v2 = CacheUtils.get("k1");
-        System.out.println("2================" + v2);
-
-        CacheUtils.expire("k1", 5);
-
-        String v3 = CacheUtils.get("k1");
-        System.out.println("3================" + v3);
-
-        Thread.sleep(8000);
-
-        String v4 = CacheUtils.get("k1");
-        System.out.println("4================" + v4);
+        System.out.println(LocationUtils.calcLocation("120.03", "43.53"));
     }
 
 
--- src/main/java/com/project/base/util/LocationUtils.java
+++ src/main/java/com/project/base/util/LocationUtils.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -21,7 +24,8 @@ public class LocationUtils {
 
     static {
         try {
-            String s = FileUtils.readTextContent(FileUtils.getResourceFilePath("files/location.json"));
+            InputStream resourceAsStream = LocationUtils.class.getClassLoader().getResourceAsStream("files/location.json");
+            String s = getStreamToStr(resourceAsStream);
             ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(s);
             Iterator<JsonNode> elements = rootNode.elements();
@@ -31,11 +35,8 @@ public class LocationUtils {
             }
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
+            throw new RuntimeException("位置文件不存在！");
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(calcLocation("120.03", "43.53"));
     }
 
     /**
@@ -71,6 +72,16 @@ public class LocationUtils {
         //维度差的平方
         BigDecimal powLat = a.getLatitude().subtract(b.getLatitude()).pow(2);
         return Math.sqrt(Double.parseDouble(powLon.add(powLat).toString()));
+    }
+
+    private static String getStreamToStr(InputStream tInputStream) throws Exception {
+        BufferedReader tBufferedReader = new BufferedReader(new InputStreamReader(tInputStream));
+        StringBuilder builder = new StringBuilder();
+        String sTempOneLine;
+        while ((sTempOneLine = tBufferedReader.readLine()) != null) {
+            builder.append(sTempOneLine);
+        }
+        return builder.toString();
     }
 
     private static class LocationInfo {
